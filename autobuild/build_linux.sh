4076be56e185042b221c03abc736588876a11cbd
Preston Jennings <prestonj@gmail.com>
2016-06-09 16:04:42 -0400

    Using proper linux sed instead of apple version

--- autobuild/build_linux.sh
+++ autobuild/build_linux.sh
@@ -48,9 +48,8 @@ cp -r ../include/* artifacts/brainCloud/include
 
 cp docs/README.TXT artifacts/brainCloud
 pushd artifacts/brainCloud
-sed -i xxx "s/Platform: xxx/Platform: Linux C++/g" README.TXT
-sed -i xxx "s/Version: x.x.x/Version: ${build_version}/g" README.TXT
-rm -f README.TXTxxx
+sed -i "s/Platform: xxx/Platform: Linux C++/g" README.TXT
+sed -i "s/Version: x.x.x/Version: ${build_version}/g" README.TXT
 popd
 
 pushd artifacts
