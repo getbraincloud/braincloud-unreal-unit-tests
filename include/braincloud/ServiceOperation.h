8b9c45f178d319c869847544ff25ec0e2b16863d
bit-jonathanm <jonathanm@bitheads.com>
2017-05-03 17:26:44 -0400

    Adding missing service variable declarations

--- include/braincloud/ServiceOperation.h
+++ include/braincloud/ServiceOperation.h
@@ -46,6 +46,8 @@ namespace BrainCloud {
 		static const ServiceOperation SendNormalizedToGroup;
 		static const ServiceOperation SendNormalized;
 		static const ServiceOperation SendNormalizedBatch;
+		static const ServiceOperation ScheduleRichNotification;
+		static const ServiceOperation ScheduleNormalizedNotification;
 
 		static const ServiceOperation Trigger;
 		static const ServiceOperation TriggerMultiple;
