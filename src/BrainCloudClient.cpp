7918821a1826786edfe0f47aaf5f5d6b662b92a1
Jonathan McCaffrey <jonathanm@bitheads.com>
2017-10-09 17:30:01 -0400

    Updating Version Numbers

--- src/BrainCloudClient.cpp
+++ src/BrainCloudClient.cpp
@@ -42,7 +42,7 @@ namespace BrainCloud {
 
 	// Define all static member variables.
 	BrainCloudClient * BrainCloudClient::_instance = NULL;
-	std::string BrainCloudClient::s_brainCloudClientVersion = "3.5.2";
+	std::string BrainCloudClient::s_brainCloudClientVersion = "3.6.0";
 
 	/**
 	 * Constructor
