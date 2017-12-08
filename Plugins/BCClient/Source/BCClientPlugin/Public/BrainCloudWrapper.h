22f36bdd9c5ecb491264ad6b386ecec759ee130b
bit-jonathanm <jonathanm@bitheads.com>
2017-12-08 13:37:45 -0500

    Adding Deprecation Message

--- Plugins/BCClient/Source/BCClientPlugin/Public/BrainCloudWrapper.h
+++ Plugins/BCClient/Source/BCClientPlugin/Public/BrainCloudWrapper.h
@@ -56,7 +56,10 @@ public:
     /**
      * Method returns a singleton instance of the BrainCloudWrapper.
      * @return A singleton instance of the BrainCloudWrapper.
+	 *
+	 * @deprecated Use of the *singleton* has been deprecated. We recommend that you create your own *variable* to hold an instance of the brainCloudWrapper. Explanation here: http://getbraincloud.com/apidocs/release-3-6-5/
      */
+	DEPRECATED("3.6.5", "Use of the *singleton* has been deprecated. We recommend that you create your own *variable* to hold an instance of the brainCloudWrapper. Explanation here: http://getbraincloud.com/apidocs/release-3-6-5/")
     static BrainCloudWrapper* getInstance();
 
     /**
