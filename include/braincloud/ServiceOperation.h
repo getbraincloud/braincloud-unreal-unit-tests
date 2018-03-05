81b112bd91e3278886fa9594774df44ee2b83e6a
Jonathan McCaffrey <jonathanm@bitheads.com>
2018-03-05 14:27:09 -0500

    Change email identity WIP

--- include/braincloud/ServiceOperation.h
+++ include/braincloud/ServiceOperation.h
@@ -20,6 +20,9 @@ namespace BrainCloud {
 		static const ServiceOperation GetIdentities;
 		static const ServiceOperation GetExpiredIdentities;
 		static const ServiceOperation RefreshIdentity;
+		static const ServiceOperation ChangeEmailIdentity;
+
+
 		static const ServiceOperation AttachParentWithIdentity;
 		static const ServiceOperation AttachPeerProfile;
 		static const ServiceOperation DetachPeer;
