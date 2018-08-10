2df483225e167d2469c8dfb24c2e1d5e25737887
stevejNewPc <stevej@bitheads.com>
2018-08-10 14:30:36 -0400

    Lobby Service and Proxy
    Lobby Service and Proxy

--- Plugins/BCClient/Source/BCClientPlugin/Private/BlueprintProxies/BCMessagingProxy.h
+++ Plugins/BCClient/Source/BCClientPlugin/Private/BlueprintProxies/BCMessagingProxy.h
@@ -22,8 +22,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
 	* Service Operation - DELETE_MESSAGES
     *
     * @param in_msgBox The msgBox that the msgIds belong to
-	* @param in_msgsIds Array of msg Ids to delete
-	* @param in_callback Method to be invoked when the server response is received.
+	* @param in_msgsIds Array of msg Ids to delete	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *DeleteMessages(ABrainCloudActor *brainCloud, const FString &in_msgBox, const TArray<FString> &in_msgsIds);
@@ -33,8 +32,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
     * 
     * Service Name - messaging
 	* Service Operation - GET_MESSAGE_BOXES
-    *
-	* @param in_callback Method to be invoked when the server response is received.
+    *	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *GetMessageBoxes(ABrainCloudActor *brainCloud);
@@ -44,8 +42,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
     * 
     * Service Name - messaging
 	* Service Operation - GET_MESSAGE_COUNTS
-    *
-	* @param in_callback Method to be invoked when the server response is received.
+    *	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *GetMessageCounts(ABrainCloudActor *brainCloud);
@@ -57,8 +54,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
 	* Service Operation - GET_MESSAGES
     *
     * @param in_msgBox The msgBox that the msgIds belong to
-	* @param in_msgsIds Array of msg Ids to delete
-	* @param in_callback Method to be invoked when the server response is received.
+	* @param in_msgsIds Array of msg Ids to delete	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *GetMessages(ABrainCloudActor *brainCloud, const FString &in_msgBox, const TArray<FString> &in_msgsIds);
@@ -69,8 +65,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
     * Service Name - messaging
 	* Service Operation - GET_MESSAGES_PAGE
     *
-    * @param in_context 
-	* @param in_callback Method to be invoked when the server response is received.
+    * @param in_context 	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *GetMessagesPage(ABrainCloudActor *brainCloud, const FString &in_context);
@@ -83,7 +78,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
     *
     * @param in_context 
     * @param in_pageOffset 
-	* @param in_callback Method to be invoked when the server response is received.
+	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *GetMessagesPageOffset(ABrainCloudActor *brainCloud, const FString &in_context, int32 in_pageOffset);
@@ -95,8 +90,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
 	* Service Operation - MARK_MESSAGES_READ
     *
     * @param in_msgBox The msgBox that the msgIds belong to
-	* @param in_msgsIds Array of msg Ids to mark as read
-	* @param in_callback Method to be invoked when the server response is received.
+	* @param in_msgsIds Array of msg Ids to mark as read	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *MarkMessagesRead(ABrainCloudActor *brainCloud, const FString &in_msgBox, const TArray<FString> &in_msgsIds);
@@ -109,8 +103,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
     *
     * @param in_toProfileIds Array of recipients for the message
 	* @param in_messageText Messsage Text
-    * @param in_messageSubject Messsage subject
-	* @param in_callback Method to be invoked when the server response is received.
+    * @param in_messageSubject Messsage subject	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *SendMessage(ABrainCloudActor *brainCloud, const TArray<FString> &in_toProfileIds, const FString &in_messageText, const FString &in_messageSubject);
@@ -122,8 +115,7 @@ class UBCMessagingProxy : public UBCBlueprintCallProxyBase
 	* Service Operation - SEND_MESSAGE_SIMPLE
     *
     * @param in_toProfileIds Array of recipients for the message
-	* @param in_messageText Messsage Text
-	* @param in_callback Method to be invoked when the server response is received.
+	* @param in_messageText Messsage Text	
     */
     UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Messaging")
     static UBCMessagingProxy *SendMessageSimple(ABrainCloudActor *brainCloud, const TArray<FString> &in_toProfileIds, const FString &in_messageText);
--- Plugins/BCClient/Source/BCClientPlugin/Private/BrainCloudLobby.cpp
+++ Plugins/BCClient/Source/BCClientPlugin/Private/BrainCloudLobby.cpp
@@ -7,4 +7,147 @@
 #include "ServerCall.h"
 #include "JsonUtil.h"
 
-BrainCloudLobby::BrainCloudLobby(BrainCloudClient* client) : _client(client) {};
+BrainCloudLobby::BrainCloudLobby(BrainCloudClient *client) : _client(client){};
+
+void BrainCloudLobby::findLobby(const FString &in_roomType, int32 in_rating, int32 in_maxSteps,
+                                const FString &in_algoJson, const FString &in_filterJson, int32 in_timeoutSecs,
+                                bool in_isReady, const FString &in_extraJson, const FString &in_teamCode, const TArray<FString> &in_otherUserCxIds,
+                                IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyRoomType.getValue(), in_roomType);
+    message->SetNumberField(OperationParam::LobbyRating.getValue(), in_rating);
+    message->SetNumberField(OperationParam::LobbyMaxSteps.getValue(), in_maxSteps);
+    message->SetObjectField(OperationParam::LobbyAlgorithm.getValue(), JsonUtil::jsonStringToValue(in_algoJson));
+    message->SetObjectField(OperationParam::LobbyFilterJson.getValue(), JsonUtil::jsonStringToValue(in_filterJson));
+    message->SetNumberField(OperationParam::LobbyTimeoutSeconds.getValue(), in_timeoutSecs);
+    message->SetBoolField(OperationParam::LobbyIsReady.getValue(), in_isReady);
+    message->SetObjectField(OperationParam::LobbyExtraJson.getValue(), JsonUtil::jsonStringToValue(in_extraJson));
+    message->SetStringField(OperationParam::LobbyTeamCode.getValue(), in_teamCode);
+    message->SetArrayField(OperationParam::LobbyOtherUserCxIds.getValue(), JsonUtil::arrayToJsonArray(in_otherUserCxIds));
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::FindLobby, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::createLobby(const FString &in_roomType, int32 in_rating, int32 in_maxSteps,
+                                  bool in_isReady, const FString &in_extraJson, const FString &in_teamCode,
+                                  const FString &in_configJson, const TArray<FString> &in_otherUserCxIds, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyRoomType.getValue(), in_roomType);
+    message->SetNumberField(OperationParam::LobbyRating.getValue(), in_rating);
+    message->SetNumberField(OperationParam::LobbyMaxSteps.getValue(), in_maxSteps);
+    message->SetBoolField(OperationParam::LobbyIsReady.getValue(), in_isReady);
+    message->SetObjectField(OperationParam::LobbyExtraJson.getValue(), JsonUtil::jsonStringToValue(in_extraJson));
+    message->SetStringField(OperationParam::LobbyTeamCode.getValue(), in_teamCode);
+
+    message->SetObjectField(OperationParam::LobbySettings.getValue(), JsonUtil::jsonStringToValue(in_configJson));
+    message->SetArrayField(OperationParam::LobbyOtherUserCxIds.getValue(), JsonUtil::arrayToJsonArray(in_otherUserCxIds));
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::CreateLobby, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::findOrCreateLobby(const FString &in_roomType, int32 in_rating, int32 in_maxSteps,
+                                        const FString &in_algoJson, const FString &in_filterJson, int32 in_timeoutSecs,
+                                        bool in_isReady, const FString &in_extraJson, const FString &in_teamCode,
+                                        const FString &in_configJson, const TArray<FString> &in_otherUserCxIds, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyRoomType.getValue(), in_roomType);
+    message->SetNumberField(OperationParam::LobbyRating.getValue(), in_rating);
+    message->SetNumberField(OperationParam::LobbyMaxSteps.getValue(), in_maxSteps);
+    message->SetObjectField(OperationParam::LobbyAlgorithm.getValue(), JsonUtil::jsonStringToValue(in_algoJson));
+    message->SetObjectField(OperationParam::LobbyFilterJson.getValue(), JsonUtil::jsonStringToValue(in_filterJson));
+    message->SetNumberField(OperationParam::LobbyTimeoutSeconds.getValue(), in_timeoutSecs);
+    message->SetBoolField(OperationParam::LobbyIsReady.getValue(), in_isReady);
+    message->SetObjectField(OperationParam::LobbyExtraJson.getValue(), JsonUtil::jsonStringToValue(in_extraJson));
+    message->SetStringField(OperationParam::LobbyTeamCode.getValue(), in_teamCode);
+    message->SetObjectField(OperationParam::LobbySettings.getValue(), JsonUtil::jsonStringToValue(in_configJson));
+    message->SetArrayField(OperationParam::LobbyOtherUserCxIds.getValue(), JsonUtil::arrayToJsonArray(in_otherUserCxIds));
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::FindOrCreateLobby, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::getLobbyData(const FString &in_lobbyID, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::GetLobbyData, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::updateReady(const FString &in_lobbyID, bool in_isReady, const FString &in_extraJson, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+    message->SetBoolField(OperationParam::LobbyIsReady.getValue(), in_isReady);
+    message->SetObjectField(OperationParam::LobbyExtraJson.getValue(), JsonUtil::jsonStringToValue(in_extraJson));
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::UpdateReady, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::updateLobbyConfig(const FString &in_lobbyID, const FString &in_configJson, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+    message->SetObjectField(OperationParam::LobbySettings.getValue(), JsonUtil::jsonStringToValue(in_configJson));
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::UpdateLobbyConfig, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::switchTeam(const FString &in_lobbyID, const FString &in_teamCode, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+    message->SetStringField(OperationParam::LobbyTeamCode.getValue(), in_teamCode);
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::SwitchTeam, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::sendSignal(const FString &in_lobbyID, const FString &in_signalJson, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+    message->SetObjectField(OperationParam::LobbySignalData.getValue(), JsonUtil::jsonStringToValue(in_signalJson));
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::SendSignal, message, in_callback);
+    _client->sendRequest(sc);
+}
+/*
+void BrainCloudLobby::joinLobby(const FString &in_lobbyID, const FString &in_teamCode, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+    message->SetBoolField(OperationParam::LobbyIsReady.getValue(), in_isReady);
+    message->SetStringField(OperationParam::LobbyTeamCode.getValue(), in_teamCode);
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::JoinLobby, message, in_callback);
+    _client->sendRequest(sc);
+}
+*/
+
+void BrainCloudLobby::leaveLobby(const FString &in_lobbyID, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::LeaveLobby, message, in_callback);
+    _client->sendRequest(sc);
+}
+
+void BrainCloudLobby::removeMember(const FString &in_lobbyID, const FString &in_connectionId, IServerCallback *in_callback)
+{
+    TSharedRef<FJsonObject> message = MakeShareable(new FJsonObject());
+    message->SetStringField(OperationParam::LobbyIdentifier.getValue(), in_lobbyID);
+    message->SetStringField(OperationParam::LobbyConnectionId.getValue(), in_connectionId);
+
+    ServerCall *sc = new ServerCall(ServiceName::Lobby, ServiceOperation::RemoveMember, message, in_callback);
+    _client->sendRequest(sc);
+}
