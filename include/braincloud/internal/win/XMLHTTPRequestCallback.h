c3fde7c056adeffb151574caa72075405fdf231a
davidstl <davidstl@bitheads.com>
2018-10-18 08:46:32 -0400

    Replaced IXMLHttpRequest for IXMLHttpRequest2

---
+++ include/braincloud/internal/win/XMLHTTPRequestCallback.h
@@ -0,0 +1,57 @@
+#ifndef _XMLHTTPREQUESTCALLBACK_H_
+#define _XMLHTTPREQUESTCALLBACK_H_
+
+#include <functional>
+
+#include <atlbase.h>
+#include <msxml6.h>
+
+namespace BrainCloud
+{
+    class XMLHTTPRequestCallback : public IXMLHTTPRequest2Callback
+    {
+    public:
+        XMLHTTPRequestCallback(
+            const std::function<void(const std::string&, int)>& onSuccess,
+            const std::function<void(int)>& onError);
+        virtual ~XMLHTTPRequestCallback();
+
+        HRESULT STDMETHODCALLTYPE QueryInterface(
+            REFIID riid,
+            _COM_Outptr_ void __RPC_FAR *__RPC_FAR *ppvObj) override;
+
+        ULONG STDMETHODCALLTYPE AddRef(void) override;
+
+        ULONG STDMETHODCALLTYPE Release(void) override;
+
+        HRESULT STDMETHODCALLTYPE OnRedirect(
+            /* [in] */ __RPC__in_opt IXMLHTTPRequest2 *pXHR,
+            /* [string][in] */ __RPC__in_string const WCHAR *pwszRedirectUrl) override;
+
+        HRESULT STDMETHODCALLTYPE OnHeadersAvailable(
+            /* [in] */ __RPC__in_opt IXMLHTTPRequest2 *pXHR,
+            /* [in] */ DWORD dwStatus,
+            /* [string][in] */ __RPC__in_string const WCHAR *pwszStatus) override;
+
+        HRESULT STDMETHODCALLTYPE OnDataAvailable(
+            /* [in] */ __RPC__in_opt IXMLHTTPRequest2 *pXHR,
+            /* [in] */ __RPC__in_opt ISequentialStream *pResponseStream) override;
+
+        HRESULT STDMETHODCALLTYPE OnResponseReceived(
+            /* [in] */ __RPC__in_opt IXMLHTTPRequest2 *pXHR,
+            /* [in] */ __RPC__in_opt ISequentialStream *pResponseStream) override;
+
+        HRESULT STDMETHODCALLTYPE OnError(
+            /* [in] */ __RPC__in_opt IXMLHTTPRequest2 *pXHR,
+            /* [in] */ HRESULT hrError) override;
+
+    private:
+        LONG _refCount;
+        int _httpStatus;
+
+        std::function<void(const std::string&, int)> _onSuccess;
+        std::function<void(int)> _onError;
+    };
+};
+
+#endif /* _XMLHTTPREQUESTCALLBACK_H_ */
--- src/win/XMLHTTPRequestLoader.cpp
+++ src/win/XMLHTTPRequestLoader.cpp
@@ -1,5 +1,5 @@
 #include "braincloud/internal/URLRequestMethod.h"
-#include "braincloud/internal/win/XMLHTTPEventSink.h"
+#include "braincloud/internal/win/XMLHTTPRequestCallback.h"
 #include "braincloud/internal/win/XMLHTTPRequestLoader.h"
 
 #include "braincloud/http_codes.h"
@@ -18,8 +18,169 @@ static std::string utf16ToUtf8(const std::wstring& utf16)
     return stringConverter.to_bytes(utf16);
 }
 
+static std::wstring utf8ToUtf16(const std::string& utf8)
+{
+    static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> stringConverter;
+    return stringConverter.from_bytes(utf8);
+}
+
 namespace BrainCloud
 {
+    class DataStream final : public ISequentialStream, public IDispatch
+    {
+    public:
+        DataStream(const std::string& data)
+            : _refCount(1)
+            , _data((const uint8_t*)data.data())
+            , _size((ULONG)data.size())
+            , _read(0)
+        {
+        }
+
+        HRESULT STDMETHODCALLTYPE QueryInterface(
+            REFIID riid,
+            _COM_Outptr_ void __RPC_FAR *__RPC_FAR *ppvObj) override
+        {
+            if (!ppvObj)
+            {
+                return E_INVALIDARG;
+            }
+            *ppvObj = NULL;
+            if (riid == IID_IUnknown || riid == IID_ISequentialStream || riid == IID_IDispatch)
+            {
+                // Increment the reference count and return the pointer.
+                *ppvObj = (LPVOID)this;
+                AddRef();
+                return NOERROR;
+            }
+            return E_NOINTERFACE;
+        }
+
+        ULONG STDMETHODCALLTYPE AddRef(void) override
+        {
+            InterlockedIncrement(&_refCount);
+            return _refCount;
+        }
+
+        ULONG STDMETHODCALLTYPE Release(void) override
+        {
+            // Decrement the object's internal counter.
+            ULONG ulRefCount = InterlockedDecrement(&_refCount);
+            if (0 == _refCount)
+            {
+                delete this;
+            }
+            return ulRefCount;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetTypeInfoCount(
+            /* [out] */ __RPC__out UINT *pctinfo) override
+        {
+            if (pctinfo)
+            {
+                *pctinfo = 0;
+            }
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetTypeInfo(
+            /* [in] */ UINT iTInfo,
+            /* [in] */ LCID lcid,
+            /* [out] */ __RPC__deref_out_opt ITypeInfo **ppTInfo) override
+        {
+            if (ppTInfo)
+            {
+                *ppTInfo = 0;
+            }
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetIDsOfNames(
+            /* [in] */ __RPC__in REFIID riid,
+            /* [size_is][in] */ __RPC__in_ecount_full(cNames) LPOLESTR *rgszNames,
+            /* [range][in] */ __RPC__in_range(0, 16384) UINT cNames,
+            /* [in] */ LCID lcid,
+            /* [size_is][out] */ __RPC__out_ecount_full(cNames) DISPID *rgDispId) override
+        {
+            return DISP_E_UNKNOWNNAME;
+        }
+
+        HRESULT STDMETHODCALLTYPE Invoke(
+            /* [annotation][in] */
+            _In_  DISPID dispIdMember,
+            /* [annotation][in] */
+            _In_  REFIID riid,
+            /* [annotation][in] */
+            _In_  LCID lcid,
+            /* [annotation][in] */
+            _In_  WORD wFlags,
+            /* [annotation][out][in] */
+            _In_  DISPPARAMS *pDispParams,
+            /* [annotation][out] */
+            _Out_opt_  VARIANT *pVarResult,
+            /* [annotation][out] */
+            _Out_opt_  EXCEPINFO *pExcepInfo,
+            /* [annotation][out] */
+            _Out_opt_  UINT *puArgErr) override
+        {
+            return S_OK;
+        }
+
+        HRESULT STDMETHODCALLTYPE Read(
+            _Out_writes_bytes_to_(cb, *pcbRead)  void *pv,
+            _In_  ULONG cb,
+            _Out_opt_  ULONG *pcbRead) override
+        {
+            if (pv == nullptr)
+            {
+                return E_INVALIDARG;
+            }
+
+            auto toRead = std::min<ULONG>(_size - _read, cb);
+            if (toRead)
+            {
+                memcpy(pv, _data, toRead);
+                _data += toRead;
+            }
+            *pcbRead = toRead;
+
+            _mutex.lock();
+            _read += toRead;
+            _mutex.unlock();
+
+            if (toRead < cb)
+            {
+                return S_FALSE;
+            }
+
+            return S_OK;
+        }
+
+        HRESULT STDMETHODCALLTYPE Write(
+            _In_reads_bytes_(cb)  const void *pv,
+            _In_  ULONG cb,
+            _Out_opt_  ULONG *pcbWritten) override
+        {
+            return S_OK;
+        }
+
+        ULONG getByteRead()
+        {
+            ULONG ret = 0;
+            _mutex.lock();
+            ret = _read;
+            _mutex.unlock();
+            return ret;
+        }
+
+    private:
+        std::mutex _mutex;
+        ULONG _refCount;
+        const uint8_t* _data;
+        ULONG _size;
+        ULONG _read;
+    };
+
     URLLoader* URLLoader::create()
     {
         return new XMLHTTPRequestLoader();
@@ -28,6 +189,7 @@ namespace BrainCloud
     long XMLHTTPRequestLoader::_timeoutInterval = 0;
 
     XMLHTTPRequestLoader::XMLHTTPRequestLoader()
+        : _pRequest(nullptr)
     {
     }
 
@@ -39,10 +201,10 @@ namespace BrainCloud
     void XMLHTTPRequestLoader::close()
     {
         _requestMutex.lock();
-        if (_request)
+        if (_pRequest)
         {
-            _request->abort();
-            _request = NULL;
+            _pRequest->Abort();
+            _pRequest = NULL;
         }
         _requestMutex.unlock();
     }
@@ -67,34 +229,35 @@ namespace BrainCloud
         return NULL;
     }
 
-    static void setXMLHTTPRequestHeaders(XMLHTTPRequestLoader* pLoader, CComPtr<IXMLHTTPRequest>& request)
+    static void setXMLHTTPRequestHeaders(XMLHTTPRequestLoader* pLoader, IXMLHTTPRequest2* request)
     {
         // Set the headers
         std::vector<URLRequestHeader> h = pLoader->getRequest().getHeaders();
         for (std::string::size_type i = 0; i < h.size(); i++)
         {
-            const std::string& headerName = h[i].getName();
-            const std::string& headerValue = h[i].getValue();
+            auto headerName = utf8ToUtf16(h[i].getName());
+            auto headerValue = utf8ToUtf16(h[i].getValue());
 
-            request->setRequestHeader(_bstr_t(headerName.c_str()), _bstr_t(headerValue.c_str()));
+            request->SetRequestHeader(headerName.c_str(), headerValue.c_str());
         }
 
         // Content Type (special header)
-        request->setRequestHeader(_bstr_t("Content-type"), _bstr_t(pLoader->getRequest().getContentType().c_str()));
+        request->SetRequestHeader(
+            L"Content-type",
+            utf8ToUtf16(pLoader->getRequest().getContentType()).c_str());
 
         // User Agent (special header)
         if (pLoader->getRequest().getUserAgent().length() > 0)
         {
-            request->setRequestHeader(_bstr_t("User-Agent"), _bstr_t(pLoader->getRequest().getUserAgent().c_str()));
+            request->SetRequestHeader(
+                L"User-Agent",
+                utf8ToUtf16(pLoader->getRequest().getUserAgent()).c_str());
         }
     }
 
     void XMLHTTPRequestLoader::loadThreadXMLHTTPRequest(XMLHTTPRequestLoader* pLoader)
     {
         HRESULT hr;
-        CComPtr<IXMLHTTPRequest> request;
-
-        CoInitializeEx(NULL, COINIT_MULTITHREADED);
 
         auto urlRequest = pLoader->getRequest();
         auto url = urlRequest.getUrl();
@@ -103,110 +266,73 @@ namespace BrainCloud
         bool hasTimeout = _timeoutInterval > 0;
 
         bool isCompleted = false;
+        std::string result;
+        int httpStatus = HTTP_CLIENT_NETWORK_ERROR;
         std::mutex mutex;
         std::condition_variable cv;
-        IDispatch *pSink;
 
-        // Create the request
-        if (method == URLRequestMethod::GET)
+        XMLHTTPRequestCallback* pCallback = new XMLHTTPRequestCallback(
+            [&](const std::string& in_result, int status) // Success
         {
-            if (data.length() > 0)
-            {
-                // Append the request data to the end of the URL.
-                url.append("?");
-                url.append(data);
-            }
+            std::unique_lock<std::mutex> lock(mutex);
+            isCompleted = true;
+            result = in_result;
+            httpStatus = status;
+            cv.notify_all();
+        },
+            [&](int status) // Error
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            isCompleted = true;
+            httpStatus = status;
+            cv.notify_all();
+        });
 
-            hr = request.CoCreateInstance(CLSID_XMLHTTP60);
+        DataStream* pStream = new DataStream(data);
+        IXMLHTTPRequest2* pRequest;
+
+        // Create the request - We only support POST
+        if (method == URLRequestMethod::POST)
+        {
+            // Create iunstances
+            CoInitializeEx(NULL, COINIT_MULTITHREADED);
+            CoCreateInstance(CLSID_FreeThreadedXMLHTTP60,
+                             NULL,
+                             CLSCTX_INPROC_SERVER,
+                             IID_PPV_ARGS(&pRequest));
 
             pLoader->_requestMutex.lock();
-            pLoader->_request = request;
+            pLoader->_pRequest = pRequest;
             pLoader->_requestMutex.unlock();
 
-            hr = request->open(_bstr_t("GET"),
-                               _bstr_t(url.c_str()), 
-                               _variant_t(VARIANT_TRUE),
-                               _variant_t(),
-                               _variant_t());
+            // Open the URL
+            auto urlw = utf8ToUtf16(url);
+            hr = pRequest->Open(L"POST",
+                                urlw.c_str(),
+                                pCallback,
+                                NULL, NULL, NULL, NULL);
 
-            setXMLHTTPRequestHeaders(pLoader, request);
-
-            // Hook up the onreadystatechange event handler
-            pSink = new XMLHTTPEventSink(request, [&]
-            {
-                // onCompleted
-                std::unique_lock<std::mutex> lock(mutex);
-                isCompleted = true;
-                cv.notify_all();
-            });
-            request->put_onreadystatechange(pSink);
-
-            // Send
-            hr = request->send(_variant_t());
-            if (hr != S_OK)
-            {
-                pLoader->_urlResponse.setStatusCode(HTTP_CLIENT_NETWORK_ERROR);
-                pLoader->_urlResponse.setReasonPhrase("Failed to send request");
-                pLoader->_threadRunning = false;
-                pLoader->_requestMutex.lock();
-                pLoader->_request = NULL;
-                pLoader->_requestMutex.unlock();
-                return;
-            }
-        }
-        else if (method == URLRequestMethod::POST)
-        {
-            hr = request.CoCreateInstance(CLSID_XMLHTTP60);
-
-            pLoader->_requestMutex.lock();
-            pLoader->_request = request;
-            pLoader->_requestMutex.unlock();
-
-            hr = request->open(_bstr_t("POST"),
-                               _bstr_t(url.c_str()), 
-                               _variant_t(VARIANT_TRUE),
-                               _variant_t(),
-                               _variant_t());
-
-            setXMLHTTPRequestHeaders(pLoader, request);
+            // Set Headers
+            setXMLHTTPRequestHeaders(pLoader, pRequest);
 
             // Set post data
-            request->setRequestHeader(_bstr_t("Content-length"), _bstr_t(std::to_string(data.size()).c_str()));
-
-            _variant_t var;
-            var.vt = VT_ARRAY | VT_UI1;
-
-            SAFEARRAYBOUND rgsabound[1];
-            rgsabound[0].cElements = (ULONG)data.size();
-            rgsabound[0].lLbound = 0;
-
-            var.parray = SafeArrayCreate(VT_UI1, 1, rgsabound); // This is cleaned up internally
-
-            void* pArrayData = NULL;
-            SafeArrayAccessData(var.parray, &pArrayData);
-            memcpy(pArrayData, data.data(), data.size());
-            SafeArrayUnaccessData(var.parray);
-
-            // Hook up the onreadystatechange event handler
-            pSink = new XMLHTTPEventSink(request, [&]
-            {
-                // onCompleted
-                std::unique_lock<std::mutex> lock(mutex);
-                isCompleted = true;
-                cv.notify_all();
-            });
-            request->put_onreadystatechange(pSink);
+            pRequest->SetRequestHeader(
+                L"Content-length",
+                std::to_wstring(data.size()).c_str());
 
             // Send
-            hr = request->send(var);
+            hr = pRequest->Send(pStream, data.size());
             if (hr != S_OK)
             {
                 pLoader->_urlResponse.setStatusCode(HTTP_CLIENT_NETWORK_ERROR);
                 pLoader->_urlResponse.setReasonPhrase("Failed to send request");
                 pLoader->_threadRunning = false;
                 pLoader->_requestMutex.lock();
-                pLoader->_request = NULL;
+                pLoader->_pRequest->Release();
+                pLoader->_pRequest = NULL;
                 pLoader->_requestMutex.unlock();
+                pCallback->Release();
+                pStream->Release();
                 return;
             }
         }
@@ -217,8 +343,10 @@ namespace BrainCloud
             pLoader->_urlResponse.setReasonPhrase("Method type not supported: " + pLoader->getRequest().getMethod());
             pLoader->_threadRunning = false;
             pLoader->_requestMutex.lock();
-            pLoader->_request = NULL;
+            pLoader->_pRequest = NULL;
             pLoader->_requestMutex.unlock();
+            pCallback->Release();
+            pStream->Release();
             return;
         }
 
@@ -230,8 +358,6 @@ namespace BrainCloud
         {
             return std::chrono::steady_clock::now() >= endTime || isCompleted;
         });
-
-        pSink->Release();
 
         // Check if we timed out
         if (!isCompleted)
@@ -245,62 +371,37 @@ namespace BrainCloud
 
             // Abort the request in a thread
             pLoader->_requestMutex.lock();
-            auto requestStopThread = std::thread([](CComPtr<IXMLHTTPRequest> request)
+            std::thread requestStopThread([=]
             {
-                if (request)
+                if (pRequest)
                 {
-                    request->abort();
+                    pRequest->Abort();
+                    pRequest->Release();
+                    pCallback->Release();
+                    pStream->Release();
                 }
-            }, pLoader->_request);
+            });
             requestStopThread.detach();
 
-            pLoader->_request = NULL;
+            pLoader->_pRequest = NULL;
             pLoader->_requestMutex.unlock();
             return;
         }
 
-        // get status
-        long status;
-        hr = request->get_status(&status);
-        if (hr != S_OK)
-        {
-            pLoader->_urlResponse.setStatusCode(HTTP_CLIENT_NETWORK_ERROR);
-            pLoader->_urlResponse.setReasonPhrase("Failed to get HTTP status");
-            pLoader->_threadRunning = false;
-            pLoader->_requestMutex.lock();
-            pLoader->_request = NULL;
-            pLoader->_requestMutex.unlock();
-            return;
-        }
-        pLoader->_urlResponse.setStatusCode((unsigned short)status);
-
-        // Get response body
-        if (status == 200)
-        {
-            VARIANT body;
-            request->get_responseBody(&body);
-            auto pData = (const uint8_t*)body.parray->pvData;
-            auto size = (size_t)(body.parray->rgsabound[0].cElements);
-            pLoader->_urlResponse.addData(std::string((char*)pData, size));
-        }
-        else
-        {
-            BSTR bstrResponse = NULL;
-            request->get_responseText(&bstrResponse);
-            std::wstring wret = L"";
-            if (bstrResponse)
-            {
-                wret = bstrResponse;
-                SysFreeString(bstrResponse);
-            }
-            pLoader->_urlResponse.setReasonPhrase(utf16ToUtf8(wret));
-        }
+        // Set results
+        pLoader->_urlResponse.setStatusCode((unsigned short)httpStatus);
+        pLoader->_urlResponse.addData(result);
 
         // We done
         pLoader->_requestMutex.lock();
-        pLoader->_request = NULL;
+        pLoader->_pRequest = NULL;
         pLoader->_requestMutex.unlock();
         pLoader->_threadRunning = false;
+
+        // Clean up
+        pRequest->Release();
+        pCallback->Release();
+        pStream->Release();
     }
 
     void XMLHTTPRequestLoader::load(URLRequest const & urlRequest)
@@ -317,5 +418,13 @@ namespace BrainCloud
     bool XMLHTTPRequestLoader::isDone()
     {
         return !_threadRunning;
+    }
+
+    void XMLHTTPRequestLoader::onError(int httpCode)
+    {
+    }
+
+    void XMLHTTPRequestLoader::onSuccess(const std::string& response, int httpCode)
+    {
     }
 }
