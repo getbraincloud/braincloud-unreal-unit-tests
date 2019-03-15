d54b7e77f482b7d83eab789203d1bfe30ec3d17d
ruth0048 <ryan.daniel.ruth@gmail.com>
2019-03-15 17:02:42 -0400

    Most recent changes to ARM building

--- Plugins/BCClient/Source/BCClientPlugin/BCClientPlugin.Build.cs
+++ Plugins/BCClient/Source/BCClientPlugin/BCClientPlugin.Build.cs
@@ -1,5 +1,6 @@
 // Copyright 2018 bitHeads, Inc. All Rights Reserved.
 
+using System.Collections.Generic;
 using System;
 using System.IO;
 using UnrealBuildTool;
@@ -159,33 +160,49 @@ public class BCClientPlugin : ModuleRules
         }
         else if(Target.Platform == UnrealTargetPlatform.Android)
         {
-            //TODO : find a way to get the engine path with UnrealBuildTool - note, have tried Fpaths and BuildConfiguration.RelativeEnginePath() and 
+             //TODO : find a way to get the engine path with UnrealBuildTool - note, have tried Fpaths and BuildConfiguration.RelativeEnginePath() and 
                 //DirectoryReference.GetCurrentDirectory(). We want to use the UE4 shipped socket lib, otherwise the static ones we save may get old. 
                 //libWebSockets for Android are located in the Engine here "/Source/ThirdParty/libWebSockets/libwebsockets/lib/Android"
             //string pathToAndroidLibWebSockets = BuildConfiguration.RelativeEnginePath() + "/Source/ThirdParty/libWebSockets/libwebsockets/lib/Android"; 
             PublicIncludePaths.Add(Path.Combine(ModulePath, "ThirdParty/include/Android/"));
 
             //we have several types of targets. Need to separate those targets when building so that the right target builds with the right libWebSockets lib
+            //string folderDir = "ThirdParty/lib/Android/";
+            //string libDir = "ThirdParty/lib/Android/";
+            //string libName = "/libwebsockets.a";
 
-            //so we want to build ARM_7 and ARM64 separately by default
-            string folderDir = "ThirdParty/lib/Android/ARMv7"; //Default to ARMv7
-            string staticLibLoc = "ThirdParty/lib/Android/ARMv7/libwebsockets.a"; //ARMv7 Location
-            
-            if(Target.Architecture == "ARM64")
+            List<string> StaticLibrariesAndroid = new List<string>();
+            // multiple supported arch types
+            // UE4 multiple arch build support broken after this engine version
+
+            System.Console.WriteLine();
+
+            #if __TARGET_ARCH_ARM == ARM64
+            System.Console.WriteLine("YOURE IN ARM 64!");
+                PublicLibraryPaths.Add(Path.Combine(ModulePath, "ThirdParty/lib/Android/" + "ARM64"));
+                StaticLibrariesAndroid.Add(Path.Combine(ModulePath, "ThirdParty/lib/Android/" + "ARM64" + "/libwebsockets.a"));
+            #endif
+
+            #if !WITH_FORWARDED_MODULE_RULES_CTOR
+            else 
+            # endif 
+
+            #if __TARGET_ARCH_ARM == mehg
+            System.Console.WriteLine("YOURE IN ARM 7!");
+                PublicLibraryPaths.Add(Path.Combine(ModulePath, "ThirdParty/lib/Android/" + "ARMv7"));
+                StaticLibrariesAndroid.Add(Path.Combine(ModulePath, "ThirdParty/lib/Android/" + "ARMv7" + "/libwebsockets.a"));
+            #endif
+
+            //if StaticLibraries has nothing in it, none of their libraries matched up, so we'll use the default ARM64 libWebSockets, 
+            //and send them a message telling them that they are using an unsupported library. 
+            if(StaticLibrariesAndroid.Count == 0)
             {
-                folderDir = "ThirdParty/lib/Android/ARM64";
-                staticLibLoc = "ThirdParty/lib/Android/ARM64/libwebsockets.a"; //ARM64 location
+                System.Console.WriteLine("Unsupported Architechture. Will attempt to build for default ARM64");
+                //default ARM64 paths.
+                PublicLibraryPaths.Add(Path.Combine(ModulePath, "ThirdParty/lib/Android/" + "ARM64"));
+                StaticLibrariesAndroid.Add(Path.Combine(ModulePath, "ThirdParty/lib/Android/" + "ARM64" + "/libwebsockets.a"));
             }
-
-            PublicLibraryPaths.AddRange(
-                new string[] {
-                    Path.Combine(ModulePath, folderDir),
-                });
-
-            string[] StaticLibrariesAndroid = new string[] {
-                    Path.Combine(ModulePath, staticLibLoc),
-            };
-
+  
             foreach (string Lib in StaticLibrariesAndroid)
             {
                 PublicAdditionalLibraries.Add(Lib);
