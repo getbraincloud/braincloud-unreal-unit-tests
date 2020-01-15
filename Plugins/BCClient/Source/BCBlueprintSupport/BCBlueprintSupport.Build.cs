e7fdc8a30165e27618c4d80198e3737d07d14c9f
Pascal <pascalf@bitheads.com>
2020-01-15 16:18:32 -0500

    finished wrapping everything in defines

--- Plugins/BCClient/Source/BCBlueprintSupport/BCBlueprintSupport.Build.cs
+++ Plugins/BCClient/Source/BCBlueprintSupport/BCBlueprintSupport.Build.cs
@@ -19,7 +19,27 @@ public class BCBlueprintSupport : ModuleRules
         PrivatePCHHeaderFile = "Private/BCBlueprintSupportPrivatePCH.h";
 
         MinFilesUsingPrecompiledHeaderOverride = 1;
+        //need to wrap
+// #if WITH_FORWARDED_MODULE_RULES_CTOR
+//     #if UE_4_24_OR_LATER
+//         bUseUnity=true;
+//     #else
+//         bFasterWithoutUnity=true;
+//     #endif
+// #else
+//         bFasterWithoutUnity=true;
+// #endif
+
+#if WITH_FORWARDED_MODULE_RULES_CTOR
+    #if UE_4_23_OR_EARLIER
+        bFasterWithoutUnity=true;
+    #if UE_4_24_OR_LATER
         bUseUnity=true;
+    #endif
+    #endif
+#endif
+
+//bFasterWithoutUnity=true;
 
         PrivateDependencyModuleNames.AddRange(
             new string[] { 
