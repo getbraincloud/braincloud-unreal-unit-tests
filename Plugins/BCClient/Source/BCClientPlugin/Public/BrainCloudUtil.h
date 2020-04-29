4a3ac79c2906f42722fbb2887774fb3140c13b78
Pascal <pascalf@bitheads.com>
2020-04-28 20:11:27 -0400

    pushing changes

--- Plugins/BCClient/Source/BCClientPlugin/Public/BrainCloudUtil.h
+++ Plugins/BCClient/Source/BCClientPlugin/Public/BrainCloudUtil.h
@@ -1,6 +1,8 @@
 // Copyright 2018 bitHeads, Inc. All Rights Reserved.
 
 #pragma once
+#include "Components/ActorComponent.h"
+#include "BrainCloudUtil.generated.h"
 
 class BrainCloudClient;
 class IServerCallback;
@@ -10,9 +12,29 @@ class BCCLIENTPLUGIN_API BrainCloudUtil
   public:
     BrainCloudUtil(BrainCloudClient *client);
 
-    static int64 ToUTCEpochTime(FDateTime dateTime);
+    static int64 UTCDateTimeToUTCMillis(FDateTime dateTime);
     static FDateTime ToDateTimeFromUTCEpoch(int64 utcDateTime);
       
   private:
     BrainCloudClient *_client = nullptr;
 };
+ //#include "UObject/NoExportTypes.h"
+//#include "Delegates/DelegateCombinations.h"
+//#pragma once
+
+
+ UCLASS(Blueprintable, BlueprintType)
+ class BCCLIENTPLUGIN_API UTimeUtilsBase : public UObject
+ {
+ 	GENERATED_BODY()
+  public:
+    UTimeUtilsBase();
+	virtual void BeginDestroy() override;
+
+
+    //static FDateTime UTCMillisToUTCDateTime(int64 utcMillis);
+
+UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|TimeUtils")
+static int64 UTCDateTimeToUTCMillisBP(FDateTime utcDate);	
+
+ };
