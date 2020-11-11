b12d5fa2f4fa6c3e26dd2e2857cc0b6debf1f2a3
ryanruth <ryanr@bitheads.com>
2020-11-11 13:42:37 -0500

    updating version

--- Plugins/BCClient/Source/BCClientPlugin/Private/BrainCloudClient.cpp
+++ Plugins/BCClient/Source/BCClientPlugin/Private/BrainCloudClient.cpp
@@ -20,7 +20,7 @@
 #include "BCPlatform.h"
 
 // Define all static member variables.
-FString BrainCloudClient::s_brainCloudClientVersion = TEXT("4.5.6");
+FString BrainCloudClient::s_brainCloudClientVersion = TEXT("4.6.0");
 
 ////////////////////////////////////////////////////
 // (De)Constructors
