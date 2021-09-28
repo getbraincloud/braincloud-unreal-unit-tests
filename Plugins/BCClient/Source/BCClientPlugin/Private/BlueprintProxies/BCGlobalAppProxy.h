0d705797a36fdb919a28c439055b05950e223ef4
francobithead <82105876+francobithead@users.noreply.github.com>
2021-09-28 09:26:46 -0400

    BCLOUD-1086 Implement ReadSelectedProperties & ReadPropertiesInCategories (#150)

--- Plugins/BCClient/Source/BCClientPlugin/Private/BlueprintProxies/BCGlobalAppProxy.h
+++ Plugins/BCClient/Source/BCClientPlugin/Private/BlueprintProxies/BCGlobalAppProxy.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "BCBlueprintCallProxyBase.h"
+#include "BrainCloudWrapper.h"
 #include "BCGlobalAppProxy.generated.h"
 
 UCLASS(MinimalAPI)
@@ -20,5 +21,29 @@ public:
     * Service Operation - ReadProperties
     */
   UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Global App")
-  static UBCGlobalAppProxy *ReadProperties(UBrainCloudWrapper *brainCloud);
+  static UBCGlobalAppProxy *ReadProperties(UBrainCloudWrapper *brainCloudWrapper);
+
+  /**
+  * Returns a list of properties, identified by the propertyIds provided.
+  * If a property from the list isn't found, it just isn't returned (no error).
+  *
+  * Service Name - GlobalApp
+  * Service Operation - ReadSelectedProperties
+  *
+  * @param - in_propertyIds Specifies which property to return
+  */
+  UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Global App")
+  static UBCGlobalAppProxy *ReadSelectedProperties(const TArray<FString>& in_propertyIds, UBrainCloudWrapper *brainCloudWrapper);
+
+  /**
+  * Returns a list of properties, identified by the categories provided.
+  * If a category from the list isn't found, it just isn't returned (no error).
+  *
+  * Service Name - GlobalApp
+  * Service Operation - ReadPropertiesInCategories
+  *
+  * @param - in_categories Specifies which category to return
+  */
+  UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "BrainCloud|Global App")
+  static UBCGlobalAppProxy *ReadPropertiesInCategories(const TArray<FString>& in_categories, UBrainCloudWrapper *brainCloudWrapper);
 };
