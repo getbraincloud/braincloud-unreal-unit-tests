56908361330110c2719f3e98208afcd9e9a25585
Joanne Hoar <joanneh@bitheads.com>
2022-07-05 15:13:52 -0600

    Restructured to be plugin only (todo: move subsystem to another repository).

--- Source/BCSubsystem/BCSubsystem.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BCSubsystem.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BCSubsystem, "BCSubsystem" );
