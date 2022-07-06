8a5075454bee5787bbcfcbb30f9280670f24af3c
Joanne Hoar <joanneh@bitheads.com>
2022-07-06 13:44:32 -0600

    Replacing plugin folders with git submodules. To view the current plugin source see https://github.com/getbraincloud/braincloud-unreal-plugin-src To view history see this branch.

--- Plugins/BCClient/Source/BCClientPlugin/Public/ConvertUtilities.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class BCCLIENTPLUGIN_API ConvertUtilities
-{
-public:
-    static FString BCBytesToString(const uint8 *in, int32 count);
-    static int32 BCStringToBytes(const FString &in_string, uint8 *out_bytes, int32 in_maxBufferSize);
-};
