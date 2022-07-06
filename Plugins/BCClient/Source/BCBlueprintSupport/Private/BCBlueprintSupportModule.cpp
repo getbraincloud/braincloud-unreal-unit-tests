2e61ae78ce117f2c1dc63c0e0f6fe7b8bc1b9d80
Joanne Hoar <joanneh@bitheads.com>
2022-07-06 13:44:32 -0600

    Replacing plugin folders with git submodules. To view the current plugin source see https://github.com/getbraincloud/braincloud-unreal-plugin-src To view history see this branch.

--- Plugins/BCClient/Source/BCBlueprintSupport/Private/BCBlueprintSupportModule.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2018 bitHeads, Inc. All Rights Reserved.
-
-#include "BCBlueprintSupportPrivatePCH.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FDefaultModuleImpl, BCBlueprintSupport);
