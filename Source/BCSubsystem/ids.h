64b6b15438da219dd92bed1f4244b787f9ae928b
Joanne Hoar <joanneh@bitheads.com>
2022-08-11 13:43:46 -0600

    Added ids.h file for running on devices.

---
+++ Source/BCSubsystem/ids.h
@@ -0,0 +1,11 @@
+/*
+ Do not commit modifications to this file in source control.
+ Keep your app secret key private.
+ Visit the BrainCloud Portal for info.
+ */
+#pragma once
+
+// internal
+#define BRAINCLOUD_SERVER_URL ""
+#define BRAINCLOUD_APP_ID ""
+#define BRAINCLOUD_APP_SECRET ""
