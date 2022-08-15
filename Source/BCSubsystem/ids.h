a5a972fdbd2455dd19aa597c8e9899f7db8f0008
Joanne Hoar <joanneh@bitheads.com>
2022-08-15 12:27:02 -0600

    Ignoring ids.h so it doesn't accidentally get committed. Please find it in our private repo.

--- Source/BCSubsystem/ids.h
+++ Source/BCSubsystem/ids.h
@@ -6,21 +6,21 @@
 #pragma once
 
 // basic client unit tests
-#define BRAINCLOUD_SERVER_URL ""
-#define BRAINCLOUD_APP_ID ""
-#define BRAINCLOUD_APP_SECRET ""
+#define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherv2"
+#define BRAINCLOUD_APP_ID "20001"
+#define BRAINCLOUD_APP_SECRET "4e51b45c-030e-4f21-8457-dc53c9a0ed5f"
 
 // extra info for some tests
-#define BRAINCLOUD_CHILD_APP_ID ""
-#define BRAINCLOUD_CHILD_APP_SECRET ""
-#define BRAINCLOUD_PARENT_LEVEL_NAME ""
-#define BRAINCLOUD_PEER_NAME ""
-#define BRAINCLOUD_SERVER_NAME ""
-#define BRAINCLOUD_SERVER_SECRET ""
-#define BRAINCLOUD_S2S_SERVER ""
+#define BRAINCLOUD_CHILD_APP_ID "20005"
+#define BRAINCLOUD_CHILD_APP_SECRET "f8cec1cf-2f95-4989-910c-8caf598f83db"
+#define BRAINCLOUD_PARENT_LEVEL_NAME "Master"
+#define BRAINCLOUD_PEER_NAME "peerapp"
+#define BRAINCLOUD_SERVER_NAME "TestServer"
+#define BRAINCLOUD_SERVER_SECRET "2ddf8355-c516-48dd-a6b0-e35bd75fac80"
+#define BRAINCLOUD_S2S_SERVER "https://sharedprod.braincloudservers.com/s2sdispatcher"
 
 // may be no longer used
-#define TESTFILE_SERVER_URL ""
-#define TESTFILE_APP_ID ""
-#define TESTFILE_APP_SECRET ""
+#define TESTFILE_SERVER_URL "https://internal.braincloudservers.com/dispatcherv2"
+#define TESTFILE_APP_ID "10299"
+#define TESTFILE_APP_SECRET "91c3a097-4697-4787-ba1c-ff6e737ff8b3"
 
