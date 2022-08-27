a658568b31f2d73fbb3c3cc2ee56da44be453a4c
Joanne Hoar <joanneh@bitheads.com>
2022-08-27 14:14:57 -0600

    Update submodule and generate ids script

--- autobuild/generate-ids.sh
+++ autobuild/generate-ids.sh
@@ -1,6 +1,9 @@
 PROJECT_NAME=${1?Error}
-APP_ID=${2?Error}
-SECRET_KEY=${3?Error}
+APP_ID=${2}
+SECRET_KEY=${3}
+CHILD_APP_ID=${4}
+CHILD_SECRET_KEY=${5}
+SERVER_SECRET_KEY=${6}
 
 cd Source/$PROJECT_NAME
 
@@ -11,14 +14,15 @@ echo "#pragma once" >ids.h
 echo "#define BRAINCLOUD_SERVER_URL \"https://api.internal.braincloudservers.com/dispatcherv2\"" >>ids.h
 echo "#define BRAINCLOUD_APP_ID \"$APP_ID\"" >>ids.h
 echo "#define BRAINCLOUD_APP_SECRET \"$SECRET_KEY\"" >>ids.h
+echo "#define BRAINCLOUD_APP_VERSION \"1.0\"" >>ids.h
 
-echo "#define BRAINCLOUD_CHILD_APP_ID \"\"" >>ids.h
-echo "#define BRAINCLOUD_CHILD_APP_SECRET \"\"" >>ids.h
-echo "#define BRAINCLOUD_PARENT_LEVEL_NAME \"\"" >>ids.h
-echo "#define BRAINCLOUD_PEER_NAME \"\"" >>ids.h
-echo "#define BRAINCLOUD_SERVER_NAME \"\"" >>ids.h
-echo "#define BRAINCLOUD_SERVER_SECRET \"\"" >>ids.h
-echo "#define BRAINCLOUD_S2S_SERVER \"\"" >>ids.h
+echo "#define BRAINCLOUD_CHILD_APP_ID \"$CHILD_APP_ID\"" >>ids.h
+echo "#define BRAINCLOUD_CHILD_APP_SECRET \"$CHILD_SECRET_KEY\"" >>ids.h
+echo "#define BRAINCLOUD_PARENT_LEVEL_NAME \"Master\"" >>ids.h
+echo "#define BRAINCLOUD_PEER_NAME \"peerapp\"" >>ids.h
+echo "#define BRAINCLOUD_SERVER_NAME \"TestServer\"" >>ids.h
+echo "#define BRAINCLOUD_SERVER_SECRET \"$SERVER_SECRET_KEY\"" >>ids.h
+echo "#define BRAINCLOUD_S2S_SERVER \"https://sharedprod.braincloudservers.com/s2sdispatcher\"" >>ids.h
 
 echo "#define TESTFILE_SERVER_URL \"\"" >>ids.h
 echo "#define TESTFILE_APP_ID \"\"" >>ids.h
