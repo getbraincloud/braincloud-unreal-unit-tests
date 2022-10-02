494fc6d5183dc1c80d0c9e12acb4ca8b60c81bec
Joanne Hoar <joanneh@bitheads.com>
2022-10-02 12:58:40 -0600

    run script for tests

--- autobuild/runtest.sh
+++ autobuild/runtest.sh
@@ -1,7 +1,8 @@
 export UNREAL_BIN="/Users/Shared/Epic Games/UE_4.27/Engine/Binaries/Mac"
 export PROJECT_NAME="BCSubsystem"
-export WORKSPACE="/Volumes/Project/release-4.11/websock/braincloud-unreal"
-export TEST_NAME=${1}
+export WORKSPACE=${1}
+export TEST_NAME=${2}
 
-"$UNREAL_BIN/UE4Editor-Cmd" "$WORKSPACE/$PROJECT_NAME.uproject" -game -nosplash -nosound -unattended -nopause -nocontentbrowser -NullRHI -ExecCmds=\"Automation RunTests $TEST_NAME\" -testexit=\"Automation Test Queue Empty\" -log -log=RunTests.log -ReportExportPath=\"$WORKSPACE/Saved/UnitTestsReport\"
+"$UNREAL_BIN/UnrealEditor-Cmd" "$WORKSPACE/$PROJECT_NAME.uproject" -game -nosplash -nosound -unattended -nopause -nocontentbrowser -NullRHI -ExecCmds=\"Automation RunTests $TEST_NAME\" -testexit=\"Automation Test Queue Empty\" -log=RunTests.log -ReportExportPath=\"$WORKSPACE/Artifacts\"
+
 
