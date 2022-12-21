92232eaf2c5ae39b6a94b1c574a87adcff90098f
jo-codegirl <joanneh@bitheads.com>
2022-12-21 13:51:33 -0700

    Updating runtest script (not fully working)

---
+++ autobuild/cleanupunreal.sh
@@ -0,0 +1,8 @@
+rm -rf Binaries
+rm -rf Intermediate
+rm -rf Plugins/BCClient/Binaries
+rm -rf Plugins/BCClient/Intermediate
+rm -rf Plugins/VaRest/Binaries
+rm -rf Plugins/VaRest/Intermediate
+echo Unreal build file have been removed.
+
--- autobuild/runtest.sh
+++ autobuild/runtest.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # usage:
-#      autobuild/runtest.sh /Volumes/Project/BrainCloudUnreal/tests-unreal-dev/ RunSet
+#      autobuild/runtest.sh RunSet
 
-export UNREAL_BIN="/Users/Shared/Epic Games/UE_4.27/Engine/Binaries/Mac"
+export UNREAL_RunUAT="/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/RunUAT.sh"
+export UNREAL_Build="/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/Mac/Build.sh"
+#export UNREAL_EditorCmd="/Users/Shared/Epic Games/UE_4.27/Engine/Binaries/Mac/ue4Editor-Cmd"
+export UNREAL_EditorCmd="/Users/Shared/Epic Games/UE_5.1/Engine/Binaries/Mac/UnrealEditor-Cmd"
 export PROJECT_NAME="BCSubsystem"
-export WORKSPACE=${1}
-export TEST_NAME=${2}
+export WORKSPACE=$PWD
+export TEST_NAME=${1}
+
+./autobuild/cleanupunreal.sh
 
 # need to build c++ source code here
+"$UNREAL_RunUAT" BuildGame -project="$WORKSPACE/$PROJECT_NAME.uproject" -compile
+ #"$UNREAL_Build" -ModuleWithSuffix=$PROJECT_NAME,8348 $PROJECT_NAME Mac Development -Project="$WORKSPACE/$PROJECT_NAME.uproject" "$WORKSPACE/$PROJECT_NAME.uproject"  -IgnoreJunk
 
-"$UNREAL_BIN/ue4Editor-Cmd" "$WORKSPACE/$PROJECT_NAME.uproject" -game -nosplash -nosound -unattended -nopause -nocontentbrowser -NullRHI -ExecCmds=\"Automation RunTests $TEST_NAME\" -testexit=\"Automation Test Queue Empty\" -log=RunTests.log -ReportExportPath=\"$WORKSPACE/Artifacts\"
+# run specified test
+"$UNREAL_EditorCmd" "$WORKSPACE/$PROJECT_NAME.uproject" -game -nosplash -nosound -unattended -nopause -nocontentbrowser -NullRHI -ExecCmds=\"Automation RunTests $TEST_NAME\" -testexit=\"Automation Test Queue Empty\" -log=RunTests.log -ReportExportPath=\"$WORKSPACE/Artifacts\"
 
 
