6552bd25100873bde18d88e0fd216dbf80b27101
jo-codegirl <joanneh@bitheads.com>
2023-01-11 11:03:16 -0500

    resolved errors in batch files

--- autobuild/cleanupunreal.bat
+++ autobuild/cleanupunreal.bat
@@ -1,13 +1,11 @@
 @ echo OFF
 
-::"Binaries","Intermediate","Plugins\BCClient\Binaries",Plugins\BCClient\Intermediate","Plugins\VaRest\Binaries","Plugins\VaRest\Intermediate"
-
-for /d %%a in ("Binaries","Intermediate") do (
+for /d %%a in ("Binaries","Intermediate","Plugins\BCClient\Binaries","Plugins\BCClient\Intermediate","Plugins\VaRest\Binaries","Plugins\VaRest\Intermediate") do (
 	if exist %%a ( 
 		set found=1
 		rmdir /S /Q  %%a
 	)
 )
-if [%found]==[1] echo Unreal build file have been removed.
+if [%found%]==[1] echo Unreal build file have been removed.
 
 exit /b %errorlevel%
