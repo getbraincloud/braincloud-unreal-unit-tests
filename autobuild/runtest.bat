7a528488d7fb1c72616e0bfa016134df2bdc6656
jo-codegirl <joanneh@bitheads.com>
2023-01-18 13:08:00 -0500

    test run script verified for windows

--- autobuild/runtest.bat
+++ autobuild/runtest.bat
@@ -1,35 +1,39 @@
-@ echo OFF
+::@ echo OFF
 :: Run commandline tests
 :: usage:
 ::      autobuild\runtest.bat
 ::      autobuild\runtest.bat RunSet
 ::      autobuild\runtest.bat Authentication clean
 
-:: set UE_RUNUAT_PATH="D:\Program Files\UE_5.0\Engine\Build\BatchFiles\RunUAT.bat"
-:: set UE_EDITOR_PATH="D:\Program Files\UE_5.0\Engine\Binaries\Win64\UE4Editor-cmd.exe"
+::eg. Jenkins
+:: 		> set UE_RUNUAT_PATH="D:\Program Files\UE_5.0\Engine\Build\BatchFiles\RunUAT.bat"
+:: 		> set UE_EDITOR_PATH="D:\Program Files\UE_5.0\Engine\Binaries\Win64\UE4Editor-cmd.exe"
+
+::eg. Local
+::		> set UE_RUNUAT_PATH="C:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat"
+:: 		> set UE_EDITOR_PATH="C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\Win64\UnrealEditor-cmd.exe"
+
 
 if not defined UE_RUNUAT_PATH goto Path_Error
-if not defined UE_INSTALL_PATH goto Path_Error
+if not defined UE_EDITOR_PATH goto Path_Error
 
 set PROJECTNAME=BCSubsystem
-if not defined WORKSPACE set WORKSPACE=%cd%
-if not defined TESTNAME (
-	set TESTNAME=%1
-	if [%TESTNAME%]==[] set TESTNAME=Test_
-)
+set WORKSPACE=%cd%
+set TESTNAME=%1
+
 :: make clean option
 ::cmd/c "autobuild\cleanupunreal.bat"
 
 :: need to build c++ source code here
-cmd /c "%UE_RUNUAT_PATH%" BuildCookRun -project="%WORKSPACE%\%PROJECTNAME%.uproject" -noP4 -platform=Win64 -clientconfig=Development -build 
+call %UE_RUNUAT_PATH% BuildCookRun -project="%WORKSPACE%\%PROJECTNAME%.uproject" -noP4 -platform=Win64 -clientconfig=Development -build 
 
 :: run specified test
-cmd /c "%UE_EDITOR_PATH%" "%WORKSPACE%\%PROJECTNAME%.uproject" -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests "%TESTNAME%";quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%WORKSPACE%\TestResults" -log -Log="RunTests.log
+call %UE_EDITOR_PATH% "%WORKSPACE%\%PROJECTNAME%.uproject" -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests %TESTNAME%;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%WORKSPACE%\TestResults" -log -Log=RunTests.log
 
 :: return code for tests
 exit /B %errorlevel%
 
 :Path_Error
-echo "Set EPIC_LAUNCHER_PATH and UE_INSTALL_PATH."
+echo "Set UE_RUNUAT_PATH and UE_EDITOR_PATH."
 
 exit /B 1
