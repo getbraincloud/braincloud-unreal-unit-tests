2f4f34764edf6db4b4b0084195eeda1715d22af9
jo-codegirl <joanneh@bitheads.com>
2023-04-04 11:56:09 -0600

    updated windows run test script

--- autobuild/runtest.bat
+++ autobuild/runtest.bat
@@ -2,8 +2,7 @@
 :: Run commandline tests
 :: usage:
 ::      autobuild\runtest.bat
-::      autobuild\runtest.bat RunSet
-::      autobuild\runtest.bat Authentication clean
+::      autobuild\runtest.bat Authentication
 
 ::eg. Jenkins
 :: 		> set UE_RUNUAT_PATH="D:\Program Files\UE_5.0\Engine\Build\BatchFiles\RunUAT.bat"
@@ -13,16 +12,16 @@
 ::		> set UE_RUNUAT_PATH="C:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat"
 :: 		> set UE_EDITOR_PATH="C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\Win64\UnrealEditor-cmd.exe"
 
+:: 		> set WORKSPACE=%cd%
 
 if not defined UE_RUNUAT_PATH goto Path_Error
 if not defined UE_EDITOR_PATH goto Path_Error
 
 set PROJECTNAME=BCSubsystem
-if not defined WORKSPACE set WORKSPACE=%cd%
-if not defined TESTNAME (
-	set TESTNAME=%1
-	if [%TESTNAME%]==[] set TESTNAME=Test_
-)
+
+set TESTNAME=%1
+if [%TESTNAME%]==[] set TESTNAME=Test_
+
 :: make clean option
 ::cmd/c "autobuild\cleanupunreal.bat"
 
@@ -30,7 +29,7 @@ if not defined TESTNAME (
 call %UE_RUNUAT_PATH% BuildCookRun -project="%WORKSPACE%\%PROJECTNAME%.uproject" -noP4 -platform=Win64 -clientconfig=Development -build 
 
 :: run specified test
-call %UE_EDITOR_PATH% "%WORKSPACE%\%PROJECTNAME%.uproject" -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests "%TESTNAME%";quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%WORKSPACE%\TestResults" -log -Log="RunTests.log
+call %UE_EDITOR_PATH% "%WORKSPACE%\%PROJECTNAME%.uproject" -nosplash -unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests %TESTNAME%" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%WORKSPACE%\TestResults" -log=RunTests.log
 
 :: return code for tests
 exit /B %errorlevel%
