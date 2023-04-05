6b9ef52b37c9c7d8b2b4eebdbf4002cdb08551b6
jo-codegirl <joanneh@bitheads.com>
2023-04-05 14:25:06 -0600

    new snapshot branch for UE 4.27 tests (keep up to date on plugin)

--- Source/BCSubsystem.Target.cs
+++ Source/BCSubsystem.Target.cs
@@ -16,7 +16,7 @@ public class BCSubsystemTarget : TargetRules
             // commandline option to avoid errors in Unreal source
             // not available in Xcode 13.2 or less (Apple clang version 13.0.0)
             bOverrideBuildEnvironment = true;
-            AdditionalCompilerArguments = "-Wno-unused-but-set-variable";
+            AdditionalCompilerArguments = "-Wno-unused-but-set-variable -Wno-bitwise-instead-of-logical";
         }
     }
 }
--- autobuild/update-submodules.sh
+++ autobuild/update-submodules.sh
@@ -29,7 +29,19 @@ do
             continue
         fi
     fi
-
+    
+    if [[ ${1} == "master" ]] ; then
+        echo modify branch to default
+        git submodule set-branch --default $i
+        git add .gitmodules
+    else
+        if [[ ${1} != "" ]] ; then
+            echo modify branch to ${1}
+            git submodule set-branch --branch ${1} $i
+            git add .gitmodules
+        fi
+    fi
+    
     STR=$(git config -f .gitmodules --get submodule.$i.branch)
     STR=${STR:="default"}
     
