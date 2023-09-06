8632f6138789e27c4a3e485b0d63fb6862b2f013
jo-codegirl <joanneh@bitheads.com>
2023-09-06 16:18:43 -0600

    checkout submodule script

--- autobuild/checkout-submodule.bat
+++ autobuild/checkout-submodule.bat
@@ -3,17 +3,16 @@
 
 if exist "Plugins\BCClient" (
     for /f %%i in ('git config -f .gitmodules --get submodule.Plugins/BCClient.branch') do set BRANCH=%%i
-    pushd Plugins/BCClient
+    pushd Plugins\BCClient
 
-	for /f %%i in ('git diff --compact-summary .') do set STR=%%i
-    if [%STR%]==[] (
-			echo "Folder has mods"
-		) else (
-		    if [%1]==[] git checkout %BRANCH%
-		    else git checkout %1
+	for /f %%i in ('git diff --compact-summary .') do set FOO=%%i
+    if [%FOO%]==[] (
+		    if [%1]==[] ( git checkout %BRANCH% ) else ( git checkout %1 )
 			git pull
-		)
-		popd				
-)
-else echo "Folder not exists"
+	) else (
+		echo Folder has mods
+		git status
+	)
+	popd
+) else (echo Folder not exists)
 
