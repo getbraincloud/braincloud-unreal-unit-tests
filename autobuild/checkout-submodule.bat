d7634e1e79a8286f82976df72a175daaff42c9f1
jo-codegirl <joanneh@bitheads.com>
2023-09-07 17:34:53 -0600

    Jenkinsfile submodule checkout

--- autobuild/checkout-submodule.bat
+++ autobuild/checkout-submodule.bat
@@ -1,18 +1,18 @@
-@ echo off
-:: && setlocal EnableDelayedExpansion
+@echo OFF
 
-if exist "Plugins\BCClient" (
-    for /f %%i in ('git config -f .gitmodules --get submodule.Plugins/BCClient.branch') do set BRANCH=%%i
-    pushd Plugins\BCClient
-
-	for /f %%i in ('git diff --compact-summary .') do set FOO=%%i
-    if [%FOO%]==[] (
-		    if [%1]==[] ( git checkout %BRANCH% ) else ( git checkout %1 )
-			git pull
-	) else (
-		echo Folder has mods
-		git status
+set BRANCH=%1
+if [%BRANCH%]==[] (
+	for /f %%i in ('git config -f .gitmodules --get submodule.Plugins/BCClient.branch') do set BRANCH=%%i
+		if [%BRANCH%]==[] (
+		set BRANCH=master
 	)
-	popd
-) else (echo Folder not exists)
+)
+
+pushd Plugins\BCClient
+
+echo Checking out %BRANCH% to %CD%
+git checkout %BRANCH%
+git pull
+
+popd
 
