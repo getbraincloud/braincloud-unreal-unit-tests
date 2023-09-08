51edad805175d9461883e1680fae4370cec93067
jo-codegirl <joanneh@bitheads.com>
2023-09-08 07:11:14 -0600

    fix logic on checkout submodules on windows

--- autobuild/checkout-submodule.bat
+++ autobuild/checkout-submodule.bat
@@ -1,13 +1,19 @@
 @echo OFF
 
+:: use command argument
 set BRANCH=%1
+
+:: if that's not set, use submodule branch in .gitmodules file
 if [%BRANCH%]==[] (
-	for /f %%i in ('git config -f .gitmodules --get submodule.Plugins/BCClient.branch') do set BRANCH=%%i
-		if [%BRANCH%]==[] (
-		set BRANCH=master
-	)
+	for /f %%i in ('git config -f .gitmodules --get submodule.Plugins/BCClient.branch') do (set BRANCH=%%i)
 )
 
+:: if that's not set, use the default branch
+if [%BRANCH%]==[] (
+	set BRANCH=master
+)
+
+:: go into submodule directory and checkout latest code
 pushd Plugins\BCClient
 
 echo Checking out %BRANCH% to %CD%
@@ -15,4 +21,3 @@ git checkout %BRANCH%
 git pull
 
 popd
-
