125b373f9a339575092ad657720927d0c1d38859
jo-codegirl <joanneh@bitheads.com>
2023-10-12 14:27:04 -0600

    updating build scripts and plugin submodule

--- autobuild/makebuild.bat
+++ autobuild/makebuild.bat
@@ -1,13 +1,38 @@
-git submodule update --remote %PROJECTNAME%/Plugins/BCClient
-rmdir /s /q %WORKSPACE%\%PROJECTNAME%_Unreal_WindowsBuild
-del /q %WORKSPACE%\%PROJECTNAME%_Unreal_WindowsBuild.zip
-mkdir %WORKSPACE%\%PROJECTNAME%_Unreal_WindowsBuild
+@ echo off
+:: usage:
+::      autobuild\makebuild.bat BCSubsystem Win64
 
-"D:\Program Files\UE_5.0\Engine\Binaries\DotNet\UnrealBuildTool\UnrealBuildTool.exe" -projectfiles -project="%WORKSPACE%\%PROJECTNAME%\%PROJECTNAME%.uproject" -game -rocket -progress 
+::eg. Jenkins (no quotes, no spaces)
+:: 		> set UE_INSTALL_PATH=C:\ProgramFiles\UE_5.0
 
-"D:\Program Files\UE_5.0\Engine\Build\BatchFiles\Build.bat" %PROJECTNAME% Win64 Development -Project="%WORKSPACE%\%PROJECTNAME%\%PROJECTNAME%.uproject" -WaitMutex -FromMsBuild
+if not defined UE_INSTALL_PATH goto Path_Error
 
-"D:\Program Files\UE_5.0\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -rocket -nocompile -compileeditor -installed -nop4 -project="%WORKSPACE%\%PROJECTNAME%\%PROJECTNAME%.uproject" -cook -stage -archive -archivedirectory="%WORKSPACE%\%PROJECTNAME%_Unreal_WindowsBuild" -package -clientconfig=Development -ue4exe=UE4Editor-Cmd.exe -clean -pak -prereqs -distribution -nodebuginfo -targetplatform=Win64 -build -utf8output 
+set PROJECTNAME=%~1
+if "%PROJECTNAME%" == "" goto Proj_Error
 
-:: zip this: ${PROJECTNAME}_Unreal_WindowsBuild
-:: archive that: ${PROJECTNAME}_Unreal_WindowsBuild.zip
+if "%~3%" == "-pack" goto BuildPlugin
+
+:BuildApp
+:: todo modify the GameDefaultMap in config file
+call "%UE_INSTALL_PATH%\Engine\Binaries\DotNet\UnrealBuildTool\UnrealBuildTool.exe" -projectfiles -project="%WORKSPACE%\%PROJECTNAME%.uproject" -game -rocket -progress
+
+call "%UE_INSTALL_PATH%\Engine\Build\BatchFiles\Build.bat" %PROJECTNAME% Win64 Development -Project="%WORKSPACE%\%PROJECTNAME%.uproject" -WaitMutex -FromMsBuild
+
+echo -- Archive folder is %WORKSPACE%\%PROJECTNAME%_Win64_%UE_VERSION%
+call "%UE_INSTALL_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -rocket -nocompile -compileeditor -installed -nop4 -project="%WORKSPACE%\%PROJECTNAME%.uproject" -unrealexe=%UE_EDITOR_CMD%.exe -cook -stage -archive -archivedirectory="%WORKSPACE%\%PROJECTNAME%_Win64_%UE_VERSION%" -package -clientconfig=Development -clean -pak -prereqs -distribution -nodebuginfo -targetplatform=Win64 -build -utf8output
+
+goto Done
+
+:BuildPlugin
+echo -- Plugin package is %WORKSPACE%\BCClient_Win64_%UE_VERSION%
+call "%UE_INSTALL_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -rocket -plugin="%WORKSPACE%\Plugins\BCClient\BCClient.uplugin" -package="%WORKSPACE%\BCClient_Win64_%UE_VERSION%" -CreateSubFolder
+
+goto Done
+
+:Done
+:: return code for tests
+exit /B %errorlevel%
+
+:Proj_Error
+echo "Pass project name."
+exit /B 1
--- autobuild/runtest.sh
+++ autobuild/runtest.sh
@@ -13,15 +13,17 @@ echo UE_EDITOR_CMD is $UE_EDITOR_CMD
 echo WORKSPACE is $WORKSPACE
 echo UE_VERSION is $UE_VERSION
 
-TEST=${1}
+TEST=${1:-Test_}
 
 # need to build c++ source code here
-"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/Mac/Build.sh" BCSubsystemEditor Mac Development -Project="$WORKSPACE/BCSubsystem.uproject" 
+#"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/Mac/Build.sh" BCSubsystemEditor Mac Development -Project="$WORKSPACE/BCSubsystem.uproject"
 
 # need to build project here
-"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -project="$WORKSPACE/BCSubsystem.uproject"   -noP4 -platform=Mac -serverconfig=Development -build 
+"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -project="$PWD/BCSubsystem.uproject" -noP4 -platform=Mac -clientconfig=Development -build
 
 # run specified test
-"${UE_INSTALL_PATH}/Engine/Binaries/Mac/${UE_EDITOR_CMD}" "$WORKSPACE/BCSubsystem.uproject"  -editortest -server -nosplash -unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests $TEST;quit" -testexit="Automation Test Queue Empty" -ue4exe="/Users/Shared/Epic Games/UE_4.27/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor" -ReportExportPath="$WORKSPACE/Mac_TestResults_UE_$UE_VERSION" -log=Mac_TestLog_UE_$UE_VERSION.log
+"${UE_INSTALL_PATH}/Engine/Binaries/Mac/${UE_EDITOR_CMD}" "$PWD/BCSubsystem.uproject"  -editortest -server -nosplash -unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests $TEST;quit" -testexit="Automation Test Queue Empty" -ReportExportPath="$PWD/TestResults_Mac_$UE_VERSION" -log=TestLog_Mac_$UE_VERSION.log
+
+cp ~/Library/Logs/Unreal\ Engine/BCSubsystemServer/TestLog_Mac_$UE_VERSION.log saved/logs/
 
 exit $?
