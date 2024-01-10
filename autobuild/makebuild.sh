6efa8ec687db1699b8d86555bfe1eb4e7ea70fdd
jo-codegirl <joanneh@bitheads.com>
2024-01-10 15:48:51 -0500

    updating build scripts from Windows

--- autobuild/makebuild.sh
+++ autobuild/makebuild.sh
@@ -15,23 +15,5 @@
 # use options nosign for android and distribution for ios
 
 PROJECTNAME=${1}
-TARGET=${2}
-GAMEMAP=${3:-AndroidTest}
 
-if [[ $UE_VERSION == "4.27" ]];
-then
-	UE4String='-ue4exe="${UE_INSTALL_PATH}/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor"'
-fi
-if [[ "$2" ==  "IOS" ]];
-then
-	ModeString='-distribution'
-fi
-
-if [[ "$3" != "-pack" ]];
-then
-
-  "${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -rocket -nocompile -game -nocompileeditor -installed -nop4 -project="$WORKSPACE/$PROJECTNAME.uproject" -cook -stage -archive -archivedirectory="$WORKSPACE/${PROJECTNAME}_${TARGET}_${UE_VERSION}" -package -clientconfig=Development -clean -pak -prereqs  ${ModeString} -nodebuginfo -targetplatform=${TARGET} -build -target=${PROJECTNAME} -utf8output
-
-else
-  "${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildPlugin -rocket -plugin="$WORKSPACE/Plugins/BCClient/BCClient.uplugin" -package="$WORKSPACE/BCClient_${TARGET}_${UE_VERSION}" -CreateSubFolder
-fi
+"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildPlugin -rocket -plugin="$WORKSPACE/Plugins/BCClient/BCClient.uplugin" -package="$WORKSPACE/artifacts/BCClient_${UE_VERSION}" -CreateSubFolder
