dd8578f11b845c948783024db08ee4baba1e4d98
jo-codegirl <joanneh@bitheads.com>
2024-01-18 14:15:47 -0700

    took out rocket option in buildplugin

--- autobuild/makebuild.bat
+++ autobuild/makebuild.bat
@@ -9,7 +9,7 @@ if not defined UE_INSTALL_PATH goto Path_Error
 
 :BuildPlugin
 echo -- Plugin package is %WORKSPACE%\artifacts\BCClient_%UE_VERSION%
-call "%UE_INSTALL_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -rocket -plugin="%WORKSPACE%\Plugins\BCClient\BCClient.uplugin" -package="%WORKSPACE%\artifacts\BCClient_%UE_VERSION%" -CreateSubFolder
+call "%UE_INSTALL_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%WORKSPACE%\Plugins\BCClient\BCClient.uplugin" -package="%WORKSPACE%\artifacts\BCClient_%UE_VERSION%" -CreateSubFolder
 
 goto Done
 
