aab44c824273ccc7d406563129e0bb1c9a403f53
Nick H <nickh@bitheads.com>
2025-08-14 11:25:27 -0400

    Fixing issue for building ue4 on MacOS with modern clang

--- Source/BCSubsystem/BCSubsystem.Build.cs
+++ Source/BCSubsystem/BCSubsystem.Build.cs
@@ -10,8 +10,12 @@ public class BCSubsystem : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "BCClientPlugin", "OnlineSubsystem", "OnlineSubsystemUtils", "Sockets" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { "Json", "OnlineSubsystem" });
-        
+
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        //For MacOS newer clang to build on UE4
+        PublicCompileWarningsOverride.Add("deprecated-builtins");
+        PublicCompileWarningsOverride.Add("bitwise-instead-of-logical");
     }
 }
