223011dfbded8cbb630cd4f6e37e322fac233d9c
Nick H <nickh@bitheads.com>
2025-08-14 14:37:40 -0400

    Removed target argument from build command

--- autobuild/runtest.sh
+++ autobuild/runtest.sh
@@ -20,7 +20,7 @@ TEST=${1:-Test_}
 #"${UE_INSTALL_PATH}/Engine/Build/BatchFiles/Mac/Build.sh" BCSubsystemEditor Mac Development -Project="$WORKSPACE/BCSubsystem.uproject"
 
 # need to build project here
-bash "${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -project="$WORKSPACE/BCSubsystem.uproject" -target=UE4Editor -noPCH -rocket -noP4 -platform=Mac -clientconfig=Development -serverconfig=Development -build -WaitMutex
+bash "${UE_INSTALL_PATH}/Engine/Build/BatchFiles/RunUAT.sh" BuildCookRun -project="$WORKSPACE/BCSubsystem.uproject" -noPCH -rocket -noP4 -platform=Mac -clientconfig=Development -serverconfig=Development -build -WaitMutex
 
 # option: list all tests
 if [[ "$TEST" == "ListAllTests" ]];
