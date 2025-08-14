4b9f9fd8441cbd274c8600386e2956b3796f9325
Nick H <nickh@bitheads.com>
2025-08-14 15:08:15 -0400

    switched compiler argument back to "-Wno-unused-but-set-variable"

--- Source/BCSubsystemEditor.Target.cs
+++ Source/BCSubsystemEditor.Target.cs
@@ -20,7 +20,7 @@ public class BCSubsystemEditorTarget : TargetRules
             // commandline option to avoid errors in Unreal source
             // not available in Xcode 13.2 or less (Apple clang version 13.0.0)
             bOverrideBuildEnvironment = true;
-            AdditionalCompilerArguments = "-Wno-unused-const-variable";
+            AdditionalCompilerArguments = "-Wno-unused-but-set-variable";
         }
     } 
 }
