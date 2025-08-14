6cb0b2935c902392ecce208a7a8e127cc14ce2bb
Nick H <nickh@bitheads.com>
2025-08-14 14:26:52 -0400

    Removed additional compiler arguments

--- Source/BCSubsystemEditor.Target.cs
+++ Source/BCSubsystemEditor.Target.cs
@@ -20,9 +20,7 @@ public class BCSubsystemEditorTarget : TargetRules
             // commandline option to avoid errors in Unreal source
             // not available in Xcode 13.2 or less (Apple clang version 13.0.0)
             bOverrideBuildEnvironment = true;
-            AdditionalCompilerArguments = "-Wno-unused-but-set-variable";
-            AdditionalCompilerArguments = (AdditionalCompilerArguments ?? "") +
-    " -Wno-deprecated-builtins -Wno-bitwise-instead-of-logical -Wno-error=deprecated-builtins";
+            AdditionalCompilerArguments = "-Wno-unused-const-variable";
         }
     } 
 }
