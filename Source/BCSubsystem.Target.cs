121c27eb90d214a5956288e67cef2f7f5a0d6c80
Nick H <nickh@bitheads.com>
2025-08-20 12:48:19 -0400

    Edited target.cs files to fix build issues on Linux

--- Source/BCSubsystem.Target.cs
+++ Source/BCSubsystem.Target.cs
@@ -11,6 +11,7 @@ public class BCSubsystemTarget : TargetRules
         ExtraModuleNames.AddRange(new string[] { "BCSubsystem" });
         //DefaultBuildSettings = BuildSettingsVersion.V4;
         //IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
         if (Target.Platform == UnrealTargetPlatform.Mac)
         {
